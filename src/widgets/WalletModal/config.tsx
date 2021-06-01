82564457d9969c0fd8b861e03a917b5ab5b33ba8
Theo <trungdao912@gmail.com>
2021-06-01 14:48:48 +0700

    chore: update supported wallet

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,10 +1,5 @@
 import Metamask from "./icons/Metamask";
-import MathWallet from "./icons/MathWallet";
-import TokenPocket from "./icons/TokenPocket";
-import TrustWallet from "./icons/TrustWallet";
 import WalletConnect from "./icons/WalletConnect";
-import BinanceChain from "./icons/BinanceChain";
-import SafePalWallet from "./icons/SafePalWallet";
 import { Config, ConnectorNames } from "./types";
 
 const connectors: Config[] = [
@@ -14,35 +9,11 @@ const connectors: Config[] = [
     connectorId: ConnectorNames.Injected,
   },
   {
-    title: "TrustWallet",
-    icon: TrustWallet,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: ConnectorNames.Injected,
-  },
-  {
     title: "WalletConnect",
     icon: WalletConnect,
     connectorId: ConnectorNames.WalletConnect,
   },
-  {
-    title: "Binance Chain Wallet",
-    icon: BinanceChain,
-    connectorId: ConnectorNames.BSC,
-  },
-  {
-    title: "SafePal Wallet",
-    icon: SafePalWallet,
-    connectorId: ConnectorNames.Injected,
-  },
+
 ];
 
 export default connectors;
