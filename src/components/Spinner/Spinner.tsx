957123da45c4d1583c751570f8a5f870f181b6e4
Theo <trungdao912@gmail.com>
2021-06-10 11:26:47 +0700

    chore: update svg photo

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,28 +1,11 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import PancakeRound from "../Svg/Icons/PancakeRound";
 import { SpinnerProps } from "./types";
-
-// const rotate = keyframes`
-//   from {
-//     transform: rotate(0deg);
-//   }
-//   to {
-//     transform: rotate(360deg);
-//   }
-// `;
 
 const Container = styled.div`
   position: relative;
 `;
-
-// const PancakeRoundIcon = styled(PancakeRound)`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   animation: ${rotate} 2s linear infinite;
-//   transform: translate3d(0, 0, 0);
-// `;
 
 const Spinner: React.FC<SpinnerProps> = () => {
   return (
