01ffa5c00df409ef0c04f18ffddd0335ed61e631
Theo <trungdao912@gmail.com>
2021-06-15 15:58:16 +0700

    chore: update icon & new background color

--- src/theme/types.ts
+++ src/theme/types.ts
@@ -56,6 +56,7 @@ export type Colors = {
   background: string;
   backgroundDisabled: string;
   backgroundAlt: string;
+  backgroundAlt2: string;
   text: string;
   textDisabled: string;
   textSubtle: string;
