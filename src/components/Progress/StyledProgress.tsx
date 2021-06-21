0ff2d2d572a6bd7cc0890df5ac68c9e76b3950ac
Theo <trungdao912@gmail.com>
2021-06-21 12:31:22 +0700

    chore: change color theme

--- src/components/Progress/StyledProgress.tsx
+++ src/components/Progress/StyledProgress.tsx
@@ -11,7 +11,7 @@ export const Bar = styled.div<BarProps>`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: ${(props) => (props.primary ? props.theme.colors.secondary : `${props.theme.colors.secondary}80`)};
+  background-color: ${(props) => (props.primary ? props.theme.colors.primary : `${props.theme.colors.secondary}80`)};
   height: 100%;
   transition: width 200ms ease;
 `;
