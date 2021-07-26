c856ad82e425d3a2efe315dbed3112df8456343e
Theo <trungdao912@gmail.com>
2021-07-26 12:01:28 +0700

    chore: adding export

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
+export { default as SecondaryButton } from "./SecondaryButton"
 export { default as IconButton } from "./IconButton";
 export * from "./ExpandableButton";
 export type { ButtonProps, BaseButtonProps, Scale as ButtonScale, Variant as ButtonVariant } from "./types";
--- src/components/ButtonMenu/index.tsx
+++ src/components/ButtonMenu/index.tsx
@@ -1,3 +1,5 @@
 export { default as ButtonMenu } from "./ButtonMenu";
 export { default as ButtonMenuItem } from "./ButtonMenuItem";
+export { default as SecondaryButtonMenu } from "./SecondaryButtonMenu";
+export { default as SecondaryMenuItem } from "./SecondaryMenuItem";
 export type { ButtonMenuProps, ButtonMenuItemProps } from "./types";
