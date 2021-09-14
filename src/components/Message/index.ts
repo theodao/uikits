95d95e71efb5671c4d2a670b78079e76cf15ed1d
trungdao912 <trungdao912@gmail.com>
2021-09-14 17:50:04 +0700

    chore: exporting new component

--- src/components/Message/index.ts
+++ src/components/Message/index.ts
@@ -1,3 +1,4 @@
 export { default as Mesage } from "./Message";
 export { default as SecondaryMessage } from "./SecondaryMessage"
+export { default as TertiaryMessage} from "./TertiaryMessage"
 export type { MessageProps } from "./types";
